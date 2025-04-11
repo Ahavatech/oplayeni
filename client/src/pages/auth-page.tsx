f93d1a31339755c635f623691f5fdfdb0a371914
Ibukunoluwa Adekunle <122817791+Ahavatech@users.noreply.github.com>
2025-04-11 15:33:14 +0100

    Update auth-page.tsx

--- client/src/pages/auth-page.tsx
+++ client/src/pages/auth-page.tsx
@@ -50,15 +50,11 @@ export default function AuthPage() {
       </CardHeader>
       <CardContent>
         <Tabs defaultValue="login" className="w-full">
-          <TabsList className="grid w-full grid-cols-2">
+          <TabsList className="grid w-full grid-cols-1">
             <TabsTrigger value="login">Login</TabsTrigger>
-            <TabsTrigger value="register">Register</TabsTrigger>
           </TabsList>
           <TabsContent value="login">
             <LoginForm />
-          </TabsContent>
-          <TabsContent value="register">
-            <RegisterForm />
           </TabsContent>
         </Tabs>
       </CardContent>
