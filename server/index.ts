5054f7d01c1c6377ebc235b54d3d1f5596bbd031
[Ibukunoluwa Adekunle] <[ibkadekunle2004@gmail.com]>
2025-05-07 23:07:22 +0100

    fixed CORS issue

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    credentials: true
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
